0ef76e62190b5a474b73477a11fd1ccab1d82118
Jesper Joergensen <jesper@heroku.com>
2011-12-29 08:12:39 -0800

    This is DescribeSObject as coming from force-wsc

--- src/test/java/com/force/api/DescribeTest.java
+++ src/test/java/com/force/api/DescribeTest.java
@@ -6,14 +6,15 @@ import org.junit.Test;
 
 public class DescribeTest {
 
-	@Test
-	public void testDescribeGlobal() {
-		ForceApi api = new ForceApi(new ApiConfig()
+	static ForceApi api = new ForceApi(new ApiConfig()
 		.setUsername(Fixture.get("username"))
 		.setPassword(Fixture.get("password"))
 		.setClientId(Fixture.get("clientId"))
 		.setClientSecret(Fixture.get("clientSecret")));
 
+	
+	@Test
+	public void testDescribeGlobal() {
 		DescribeGlobal dg = api.describeGlobal();
 		assertNotNull(dg.getSObjects().get(0));
 	}
@@ -21,14 +22,9 @@ public class DescribeTest {
 
 	@Test
 	public void testDescribeSObject() {
-		ForceApi api = new ForceApi(new ApiConfig()
-		.setUsername(Fixture.get("username"))
-		.setPassword(Fixture.get("password"))
-		.setClientId(Fixture.get("clientId"))
-		.setClientSecret(Fixture.get("clientSecret")));
-
 		DescribeSObject ds = api.describeSObject("Contact");
 		assertNotNull(ds.getName());
-		assertNotNull(ds.getFields()[0].getName());
+		assertNotNull(ds.getAllFields());
 	}
+
 }
