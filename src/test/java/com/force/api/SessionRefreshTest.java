124c4cf6ebeca7d35b2229daa244514bd2998f4c
Jesper Joergensen <jesper@heroku.com>
2011-12-31 10:32:59 -0800

    Attempt to deal with flapping sessionrefresh test

--- src/test/java/com/force/api/SessionRefreshTest.java
+++ src/test/java/com/force/api/SessionRefreshTest.java
@@ -18,8 +18,20 @@ public class SessionRefreshTest {
 		assertNotNull(api.getIdentity());
 		// This call is not available in public api
 		Auth.revokeToken(api.config, api.session.accessToken);
-		
-		assertNotNull(api.getIdentity());
+
+		Identity id = null;
+		for(int i=0;i<3;i++) {
+			try {
+				id = api.getIdentity();
+			} catch(Throwable t) {
+				System.out.println("Got "+t);
+				System.out.println("Attempt "+i+1+"/3: api failed to refresh after revoking token.");
+				try {
+					Thread.sleep(1000);
+				} catch(InterruptedException e) {}
+			}
+		}
+		assertNotNull(id);
 		
 	}
 
