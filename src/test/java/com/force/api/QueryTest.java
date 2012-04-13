f1b7484b6f4a521e916e13c486f535603abfbb9c
Anand <anand@heroku.com>
2012-04-13 11:32:17 -0700

    Added tests for relationship queries

--- src/test/java/com/force/api/QueryTest.java
+++ src/test/java/com/force/api/QueryTest.java
@@ -2,9 +2,13 @@ package com.force.api;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,5 +37,26 @@ public class QueryTest {
 		// Note, attribute names are capitalized by the Force.com REST API
 		assertNotNull(result.get(0).getName());
 	}
+	
+	
+	@Test
+	public void testRelationshipQuery() throws JsonGenerationException, JsonMappingException, IOException {
+		Account a = new Account();
+		a.setName("force-rest-api-test-account");
+		String id = api.createSObject("account", a);
+		a.setId(id);
+		Contact ct = new Contact("force@test.com","FirstName","LastName");
+		ct.setAccountId(a.id);
+		api.createSObject("Contact", ct);
+		List<Account> result = api.query(String.format("SELECT name,(select Id,Email from Contacts) FROM Account where Id='%s'",a.id),
+										 Account.class).getRecords();
+		// Note, attribute names are capitalized by the Force.com REST API
+		assertEquals(result.get(0).contacts.size(),1);
+		assertEquals(result.get(0).contacts.get(0).getEmail(),"force@test.com");
+		assertEquals(result.get(0).contacts.get(0).getFirstName(),"FirstName");
+		assertEquals(result.get(0).contacts.get(0).getLastName(),"LastName");
+		assertEquals(result.get(0).contacts.get(0).getAccountId(),a.id);
+	}
+	
 
 }
