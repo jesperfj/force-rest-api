286de851541a45c6c4994f25256f47ce48ce2f02
neil.sharpe <neil.sharpe@rl0118us.rlcorp.local>
2015-01-26 11:58:52 -0800

    enhance [BUSSRV-5188]: Add ability to get version through configuration files more easily.

--- src/main/java/com/force/api/ApiVersion.java
+++ src/main/java/com/force/api/ApiVersion.java
@@ -1,6 +1,9 @@
 package com.force.api;
 
 public enum ApiVersion {
+	V27 ("v27.0"),
+	V26 ("v26.0"),
+	V25 ("v25.0"),
 	V24 ("v24.0"),
 	V23 ("v23.0"),
 	V22 ("v22.0"), 
@@ -13,5 +16,20 @@ public enum ApiVersion {
 	}
 	
 	public String toString() { return v; }
-	
+
+	public static ApiVersion getVersion(String api) {
+		if (api == null) {
+			return DEFAULT_VERSION;
+		}
+		api = api.toLowerCase();
+		if (!api.startsWith("v")) {
+			api = "v" + api;
+		}
+		for (ApiVersion v : ApiVersion.values()) {
+			if (v.v.equals(api)) {
+				return v;
+			}
+		}
+		return DEFAULT_VERSION;
+	}
 }
