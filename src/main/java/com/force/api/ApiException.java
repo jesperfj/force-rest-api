5803cbc955b4e0be6fea3d162b7b6cfe475cc7ca
nsharpe <neil.sharpe@reachlocal.com>
2015-03-30 10:45:15 -0700

    Allow for messages returned from non error stream when status code is greater then 299

--- src/main/java/com/force/api/ApiException.java
+++ src/main/java/com/force/api/ApiException.java
@@ -1,15 +1,24 @@
 package com.force.api;
 
+import com.force.api.http.HttpResponse;
+
 public class ApiException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
 	int code;
 	String message;
+	private HttpResponse resp;
 	public ApiException(int code, String message) {
 		super();
 		this.code = code;
 		this.message = message;
+	}
+	public ApiException(int code, String message,HttpResponse resp) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.resp = resp;
 	}
 	public int getCode() {
 		return code;
@@ -18,6 +27,8 @@ public class ApiException extends RuntimeException {
 		return message;
 	}
 	
-	
+	public HttpResponse getResp(){
+		return resp;
+	}
 	
 }
