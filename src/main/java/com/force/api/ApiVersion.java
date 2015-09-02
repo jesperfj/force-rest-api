3efbefce0c0ade07ca6040a38d6fcd1c14c77b3c
Jesper Joergensen <jesper@heroku.com>
2015-09-01 22:15:25 -0700

    bump to v34

--- src/main/java/com/force/api/ApiVersion.java
+++ src/main/java/com/force/api/ApiVersion.java
@@ -1,6 +1,7 @@
 package com.force.api;
 
 public enum ApiVersion {
+	V33 ("v34.0"),
 	V33 ("v33.0"),
 	V32 ("v32.0"),
 	V31 ("v31.0"),
@@ -13,7 +14,7 @@ public enum ApiVersion {
 	V24 ("v24.0"),
 	V23 ("v23.0"),
 	V22 ("v22.0"), 
-	DEFAULT_VERSION ("v33.0");
+	DEFAULT_VERSION ("v34.0");
 
 	final String v;
 	
