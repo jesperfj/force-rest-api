69344c5f5213b93a1c8a9bec962edb3896b17977
Zacheusz Siedlecki <zacheusz.s@gmail.com>
2015-12-21 14:35:44 +0100

    v30 api version

--- src/main/java/com/force/api/ApiVersion.java
+++ src/main/java/com/force/api/ApiVersion.java
@@ -3,7 +3,8 @@ package com.force.api;
 public enum ApiVersion {
 	V24 ("v24.0"),
 	V23 ("v23.0"),
-	V22 ("v22.0"), 
+	V22 ("v22.0"),
+	V30 ("v30.0"),
 	DEFAULT_VERSION ("v23.0");
 
 	final String v;
