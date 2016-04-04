4669cc75301c15a90e236ee82512c0671efacef8
Jesper Joergensen <jesper@heroku.com>
2016-04-04 03:28:21 -0700

    v36 is now default API version

--- src/main/java/com/force/api/ApiVersion.java
+++ src/main/java/com/force/api/ApiVersion.java
@@ -1,6 +1,7 @@
 package com.force.api;
 
 public enum ApiVersion {
+	V36 ("v36.0"),
 	V35 ("v35.0"),
 	V34 ("v34.0"),
 	V33 ("v33.0"),
@@ -15,7 +16,7 @@ public enum ApiVersion {
 	V24 ("v24.0"),
 	V23 ("v23.0"),
 	V22 ("v22.0"), 
-	DEFAULT_VERSION ("v35.0");
+	DEFAULT_VERSION ("v36.0");
 
 	final String v;
 	
