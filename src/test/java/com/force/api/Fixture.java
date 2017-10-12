83475d05ab6e2f414303a84042c5dbe2db8d1c75
Jesper Joergensen <jesper@heroku.com>
2017-10-12 07:52:04 -1000

    Allow test fixture to be configured in env

--- src/test/java/com/force/api/Fixture.java
+++ src/test/java/com/force/api/Fixture.java
@@ -1,6 +1,7 @@
 package com.force.api;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
 public class Fixture {
@@ -11,12 +12,23 @@ public class Fixture {
 		try {
 			props.load(Fixture.class.getResourceAsStream("/test.properties"));
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			System.out.println("Couldn't load test.properties. Using environment.");
+			//throw new RuntimeException(e);
+			// New behavior: If file not present, set from environment and don't complain here.
+			setFromEnv(props);
 		}
 	}
 	
 	public static String get(String key) {
 		return props.getProperty(key);
+	}
+
+	private static void setFromEnv(Properties props) {
+		for(Map.Entry<String,String> entry : System.getenv().entrySet()) {
+			if(entry.getKey().startsWith("TEST_")) {
+				props.setProperty(entry.getKey().substring(5), entry.getValue());
+			}
+		}
 	}
 
 }
