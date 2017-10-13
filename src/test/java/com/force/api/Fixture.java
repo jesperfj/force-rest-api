67c34eb4b4910feac9ecad79d349e121d0b2df33
Jesper Joergensen <jesper@heroku.com>
2017-10-12 23:11:36 -1000

    environment var names and test.properties names are now the same

--- src/test/java/com/force/api/Fixture.java
+++ src/test/java/com/force/api/Fixture.java
@@ -9,23 +9,26 @@ public class Fixture {
 
 	static Properties props;
 	static {
-		props = new Properties();
-			InputStream in = Fixture.class.getResourceAsStream("/test.properties");
-			if(in!=null) {
-				System.out.println("Setting test config from test.properties resource");
-				try {
-					props.load(Fixture.class.getResourceAsStream("/test.properties"));
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			} else {
-				System.out.println("Setting test config from environment");
-				setFromEnv(props);
+		InputStream in = Fixture.class.getResourceAsStream("/test.properties");
+		if(in!=null) {
+			System.out.println("Setting test config from test.properties resource");
+			props = new Properties();
+			try {
+				props.load(Fixture.class.getResourceAsStream("/test.properties"));
+			} catch (IOException e) {
+				throw new RuntimeException(e);
 			}
+		} else {
+			System.out.println("Setting test config from environment");
+		}
 	}
 	
 	public static String get(String key) {
-		return props.getProperty(key);
+		if(props!=null) {
+			return props.getProperty(key);
+		} else {
+			return System.getenv(key);
+		}
 	}
 
 	private static void setFromEnv(Properties props) {
